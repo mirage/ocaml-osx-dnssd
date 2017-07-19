183086d846aa110b5fecf1f9d20ecf355a5a87f7
Thomas Gazagnaire <thomas@gazagnaire.org>
2017-07-19 12:27:31 +0200

    Remove META file, use topkg-jbuilder

--- pkg/pkg.ml
+++ pkg/pkg.ml
@@ -1,12 +1,3 @@
 #!/usr/bin/env ocaml
 #use "topfind"
-#require "topkg"
-open Topkg
-
-let () =
-  Pkg.describe "osx-dnssd" @@ fun c ->
-  Ok [
-    Pkg.mllib "lib/osx-dnssd.mllib";
-    Pkg.clib "lib/libosx-dnssd_stubs.clib";
-    Pkg.test  "lib_test/test" ~args:(Cmd.v "-q");
-  ]
+#require "topkg-jbuilder.auto"
